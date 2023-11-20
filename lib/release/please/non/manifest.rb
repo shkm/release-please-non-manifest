a3ff5a4d1ee74a4fd3966a458c9b27d5bf622a56
Jamie Schembri <jamie@schembri.me>
2023-11-20 13:54:26 +0100

    chore: initial commit

---
+++ lib/release/please/non/manifest.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "manifest/version"
+
+module Release
+  module Please
+    module Non
+      module Manifest
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/release/please/non/manifest/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Release
+  module Please
+    module Non
+      module Manifest
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
